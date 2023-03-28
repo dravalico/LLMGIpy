99372d90e727c78c6cd73da36291f43ab6ef2a66
Damiano <damianoravalico.uni@gmail.com>
2023-03-28 08:14:40 +0000

    Fixed some obj definitions

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+import AbstractLanguageModel
 import AlpacaModel
+import AbstractModelTester
 import PSB2ModelTester
 
 
 def main():
-    alpaca_model: AlpacaModel.AlpacaModel = AlpacaModel.AlpacaModel()
-    model_tester: PSB2ModelTester.PSB2ModelTester = PSB2ModelTester.PSB2ModelTester(
+    alpaca_model: AbstractLanguageModel.AbstractLanguageModel = AlpacaModel.AlpacaModel()
+    model_tester: AbstractModelTester.AbstractModelTester = PSB2ModelTester.PSB2ModelTester(
         1, 2000, alpaca_model
     )
     model_tester.run()
