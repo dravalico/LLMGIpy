a58c8540371d09c92af35b724744411cb9153fef
Damiano <damianoravalico.uni@gmail.com>
2023-04-27 15:33:53 +0000

    Changed the way to save JSON files

--- src/scripts/JSON_data_saver.py
+++ src/scripts/JSON_data_saver.py
@@ -8,11 +8,8 @@ FOLDER_NAME: str = str(datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
 
 def create_json_file(model_name: str,
                      problem_name: str,
-                     problems_number: int,
-                     iteration: int,
-                     model_response: str,
-                     tests_results: dict[any, any],
-                     error: str = "") -> None:
+                     problem_number: int,
+                     data: list[dict[str, any]]) -> None:
     if not os.path.isdir(BASE_PATH):
         os.mkdir(BASE_PATH)
 
@@ -20,23 +17,16 @@ def create_json_file(model_name: str,
     if not os.path.isdir(results_folder_path):
         os.mkdir(results_folder_path)
 
-    model_response = model_response.replace("    ", "\t")
     to_save: dict[str, any] = {
         "model_name": model_name,
         "problem_name": problem_name,
-        "problems_number": problems_number,
-        "iteration": iteration,
-        "model_response": model_response,
-        "results": tests_results if error == "" else {"error:": error}
+        "problems_number": problem_number,
+        "data": data
     }
     json_results = json.dumps(to_save, indent=4)
 
-    filename: str = f"{model_name}{'_problem'}{problems_number}{'.json'}"
+    filename: str = f"{model_name}{'_problem'}{problem_number}{'.json'}"
     output_file_path: str = os.path.join(results_folder_path, filename)
-    if os.path.exists(output_file_path):
-        modify_type: str = "a"
-    else:
-        modify_type: str = "w"
-    output_file = open(output_file_path, modify_type)
-    output_file.write(json_results + "\n")
+    output_file = open(output_file_path, 'w')
+    output_file.write(json_results)
     output_file.close()
