dc7340f952b3b5b6cdc59d135b1fd103eedf2cd8
Damiano <damianoravalico.uni@gmail.com>
2023-05-03 13:42:25 +0000

    Renaming

--- src/scripts/individual_formatter.py
+++ src/scripts/individual_formatter.py
@@ -1,8 +1,5 @@
 import regex
 
-_START_TAB: str = "{:"
-_END_TAB: str = ":}"
-
 
 def to_pony_individual(python_code: str) -> str:
     python_code = _remove_inline_comments(python_code)
@@ -24,16 +21,16 @@ def _remove_inline_comments(python_code: str) -> str:
 
 
 def _remove_multiline_comments(python_code: str) -> str:
-    triple_2quotes: str = '"""'
-    triple_1quotes: str = "'''"
+    triple_2q: str = '"""'
+    triple_1q: str = "'''"
     res: str = ""
     line_init: str = python_code.split('\n')[0].lstrip()
-    is_take: bool = False if line_init == triple_2quotes else True
-    is_stmn: bool = True if triple_2quotes in line_init and is_take else False
+    is_take: bool = False if line_init == triple_2q else True
+    is_stmn: bool = True if triple_2q in line_init and is_take else False
     is_take_prev: bool = is_take
     for line in python_code.split('\n'):
-        if triple_2quotes in line:
-            if line.lstrip() == triple_2quotes and not is_stmn:
+        if triple_2q in line:
+            if line.lstrip() == triple_2q and not is_stmn:
                 is_take = not is_take
             else:
                 is_stmn = not is_stmn
@@ -54,6 +51,8 @@ def _remove_empty_lines(python_code: str) -> str:
 
 
 def _substitute_tabs_with_pony_encode(python_code: str) -> str:
+    start_tab: str = "{:"
+    end_tab: str = ":}"
     tab_counter: int = 0
     tmp_tab_counter: int = 0
     res: list = []
@@ -64,17 +63,17 @@ def _substitute_tabs_with_pony_encode(python_code: str) -> str:
         if tmp_tab_counter > tab_counter:
             if index != 0:
                 res[index - 1] = res[index - 1] \
-                    .replace('\n', _START_TAB + '\n')
+                    .replace('\n', start_tab + '\n')
                 res.append(line)
             else:
-                res.append(line + _START_TAB + '\n')
+                res.append(line + start_tab + '\n')
         if tmp_tab_counter < tab_counter:
-            res[index - 1] += _END_TAB + '\n'
+            res[index - 1] += end_tab + '\n'
             res.append(line)
         if tmp_tab_counter == tab_counter:
             res.append(line)
         res += '\n'
         tab_counter = tmp_tab_counter
         index = index + 2
-    res.append(_END_TAB)
+    res.append(end_tab)
     return ''.join(res)
