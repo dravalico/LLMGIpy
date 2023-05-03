cc1e8b04a1e0e7a6a544a3254d6e243ab008f602
Damiano <damianoravalico.uni@gmail.com>
2023-05-03 14:25:07 +0000

    Renaming

--- src/testers/PSB2ModelTester.py
+++ src/testers/PSB2ModelTester.py
@@ -15,92 +15,90 @@ class PSB2ModelTester(AbstractModelTester):
     def __init__(
             self,
             model: AbstractLanguageModel,
-            test_iterations: int = 1,
-            test_data_dimension: int = 2000,
+            iterations: int = 2,
+            data_dimension: int = 2000,
     ):
         super().__init__(model)
-        self.__test_iteration = test_iterations
-        self.__test_data_dimension = test_data_dimension
-        self.__model_to_test = model
+        self.__iterations = iterations
+        self.__data_dimension = data_dimension
+        self.__model = model
 
     def run(self) -> None:
         for n_prob in range(0, len(self.__PROBLEMS)):
             print(f"{'=' * 35}Problem {(n_prob + 1):02d}{'=' * 35}")
             data: list[dict[str, any]] = list()
-            for iteration in range(0, self.__test_iteration):
+            for iteration in range(0, self.__iterations):
                 print(
-                    f"\nIteration {(iteration + 1):02d}\nAsking model '{self.__model_to_test.name}'...")
-                model_response: any = self.__model_to_test.ask(
+                    f"\nIteration {(iteration + 1):02d}\nAsking model '{self.__model.name}'...")
+                model_response: any = self.__model.ask(
                     str(self.__PROBLEMS.get("Description")[n_prob]))
                 print("\n{0}\n".format(model_response))
-                function_extracted: str = super()._extract_function_body(model_response)
-                function_name: str = super()._extract_function_name(function_extracted)
+                f_extracted: str = super()._extract_function_body(model_response)
+                f_name: str = super()._extract_function_name(f_extracted)
                 output: dict[str, any] = {
                     "iteration": (iteration + 1),
                     "model_response": super()._indentation_as_tab(model_response),
-                    "function_name": function_name,
-                    "function_extracted": super()._indentation_as_tab(function_extracted),
-                    "individual": to_pony_individual(super()._indentation_as_tab(function_extracted))
+                    "function_name": f_name,
+                    "function_extracted": super()._indentation_as_tab(f_extracted),
+                    "individual": to_pony_individual(super()._indentation_as_tab(f_extracted))
                 }
-                problem_name: str = self.__PROBLEMS.get("Problem Name")[n_prob]
-                problem_name = problem_name.replace(" ", "-").lower()
-                tests_results: dict[str, int] = None
+                prob_name: str = self.__PROBLEMS.get("Problem Name")[n_prob]
+                prob_name = prob_name.replace(" ", "-").lower()
+                res: dict[str, int] = None
                 try:
-                    exec(function_extracted, globals())
+                    exec(f_extracted, globals())
                 except Exception as e:
                     print("Error during definition:", e)
                 else:
-                    exec("function_to_test = " + function_name, globals())
+                    exec("f_to_test = " + f_name, globals())
                     print("Testing...")
                     try:
-                        tests_results = self.__test_function(
-                            function_to_test, problem_name)
+                        res = self.__test_function(f_to_test, prob_name)
                     except Exception as e:
                         print("Error during tests:", e)
                         output["error"] = str(e)
                     else:
-                        output["results"] = tests_results
+                        output["results"] = res
                         print("{:.2f}% passed".format(
-                            tests_results["test_passed"] / self.__test_data_dimension * 100))
-                        print("Test(s) passed:", tests_results["test_passed"])
-                        print("Test(s) not passed:",
-                              tests_results["test_not_passed"])
+                            res["passed"] / self.__data_dimension * 100))
+                        print("Test(s) passed:", res["passed"])
+                        print("Test(s) not passed:", res["not_passed"])
                         print("Test(s) with exception(s): " +
-                              str(tests_results["test_with_exception"]) + '\n')
+                              str(res["with_exception"]) + '\n')
                 data.append(output)
             create_json_file(
-                self.__model_to_test.name,
-                problem_name,
+                self.__model.name,
+                prob_name,
                 n_prob + 1,
                 data
             )
         print(f"{'=' * 80}\n")
 
-    def __test_function(self, function_to_test: Callable, problem_name: str) -> dict[str, int]:
+    def __test_function(self, f: Callable, prob_name: str) -> dict[str, int]:
         try:
-            (train_data, test_data) = psb2.fetch_examples(
-                "", problem_name, 0, self.__test_data_dimension)
+            (_, test_data) = psb2.fetch_examples(
+                "", prob_name, 0, self.__data_dimension)
         except:
             raise Exception("Cannot load data for test")
         else:
-            test_passed: int = 0
-            test_not_passed: int = 0
-            test_with_exception: int = 0
+            passed: int = 0
+            not_passed: int = 0
+            with_exception: int = 0
             for i in range(0, len(test_data)):
                 args: list[str] = [
                     v for k, v in test_data[i].items() if "input" in k]
                 expected_output: list[str] = [
                     v for k, v in test_data[i].items() if "output" in k]
                 try:
-                    result: any = [function_to_test(*args)]
+                    result: any = [f(*args)]
                     if result == expected_output:
-                        test_passed = test_passed + 1
+                        passed = passed + 1
                     else:
-                        test_not_passed = test_not_passed + 1
+                        not_passed = not_passed + 1
                 except:
-                    test_with_exception = test_with_exception + 1
+                    with_exception = with_exception + 1
             return {
-                "test_passed": test_passed,
-                "test_not_passed": test_not_passed,
-                "test_with_exception": test_with_exception,
+                "passed": passed,
+                "not_passed": not_passed,
+                "with_exception": with_exception,
             }
