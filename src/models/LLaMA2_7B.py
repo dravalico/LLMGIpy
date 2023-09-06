bc5ec4c9be03d4d807dd30bd63301a1a43315224
Damiano <damianoravalico.uni@gmail.com>
2023-09-06 14:47:25 +0000

    Now LLaMA2 works and minor fixes

--- src/models/LLaMA2_7B.py
+++ src/models/LLaMA2_7B.py
@@ -2,16 +2,16 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 from models.AbstractLanguageModel import AbstractLanguageModel
-import os
+import re
 
 
-class Llama2_7B(AbstractLanguageModel):
+class LLaMA2_7B(AbstractLanguageModel):
 
     def __init__(self) -> None:
-        super().__init__("Llama2_7B")
+        super().__init__("LLaMA2_7B")
 
     def ask(self, question: str) -> str:
-        question: str = (self._INTRODUCTION_TO_QUESTION + question)
+        question: str = ("Results: " + question + "\nAnswer:")
         sequences = self.__pipeline(
             question,
             do_sample=True,
@@ -21,17 +21,27 @@ class Llama2_7B(AbstractLanguageModel):
             max_length=200,
         )
         result: str = ""
+        print("=====")
         for seq in sequences:
+            print(seq['generated_text'])
             result += seq['generated_text']
-        return result
+        print("=====")
+        #print(self.__extract_response(result))
+        return self.__extract_response(result)
 
     def _load_model(self) -> None:
         super()._load_model()
         model = "meta-llama/Llama-2-7b-hf"
-        self.__tokenizer = AutoTokenizer.from_pretrained(model, token=os.getenv("HF_TOKEN"), use_auth_token=True)
+        self.__tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=True)
         self.__pipeline = transformers.pipeline(
             "text-generation",
             model=model,
             torch_dtype=torch.float16,
             device_map="auto",
         )
+
+    @staticmethod
+    def __extract_response(alpaca_response: str) -> str:
+        code_blocks = re.findall(r'begin{code}(.*?)end{code}', alpaca_response, re.DOTALL)
+        extracted_code = '\n'.join(code_blocks)
+        return extracted_code
--- src/testers/ModelTester.py
+++ src/testers/ModelTester.py
@@ -35,7 +35,7 @@ class ModelTester():
     def run(self) -> str:
         print(f"\n{'=' * 80}")
         print(f"Model '{self.__model.name}'")
-        for n_prob in [0, 4, 6, 8, 18]:
+        for n_prob in range(len(self.__problems)):
             print(f"{'=' * 35}Problem {(n_prob):02d}{'=' * 35}")
             res: Dict[str, List[str]] = self.__ask_model_and_process(n_prob)
             prob_name: str = self.__problems\
