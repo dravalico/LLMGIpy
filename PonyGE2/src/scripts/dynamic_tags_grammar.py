2dc16ab0335282b85e5101cb05891c552521cadf
giovanni.pinna96 <giovanni.pinna.17l96@gmail.com>
2024-08-12 10:14:51 +0000

    add part for clean the dynamic grammar for avoid formatting error. And bug fix for take into account <, >, <= and >= as terminal node.

--- PonyGE2/src/scripts/dynamic_tags_grammar.py
+++ PonyGE2/src/scripts/dynamic_tags_grammar.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 def is_sublist(list1, list2):
     set1, set2 = set(list1), set(list2)
@@ -6,7 +7,7 @@ def is_sublist(list1, list2):
 
 def split_file_by_pattern(file_path, pattern = r'<.*?> ::='):
     try:
-        with open(file_path, 'r') as file:
+        with open(os.path.join('..', 'grammars', file_path), 'r') as file:
             content = file.read()
         find_content = re.findall(pattern, content)
         split_content = re.split(pattern, content)[1:]
@@ -28,9 +29,12 @@ def process_tuples(tuple_list, check_list):
             for item in split_second_elem:
               if is_sublist(re.findall(r'<.*?>', item), check_list):
                 for tag_to_keep in check_list:
-                  if tag_to_keep in item or ('<' not in item and '>' not in item):
-                    valid_second_elem.append(item)
-            processed_tuples.append((first_elem, '|'.join(list(set(valid_second_elem)))))
+                  if tag_to_keep in item or ('<' not in item and '>' not in item) or ('"<"' in item or '">"' in item or '">="' in item or '"<="' in item):
+                    valid_second_elem.append(item) 
+            if first_elem != '<STRINGS> ': 
+                processed_tuples.append((first_elem, '|'.join(list(set(valid_second_elem)))))
+            else:
+                processed_tuples.append((first_elem, second_elem))
     return processed_tuples
 
 def join_tuples(tuple_list):
@@ -44,23 +48,21 @@ def save_string_to_file(string_to_save, file_path):
         print("String saved to file successfully.\n")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+        
+def clean_string(input_string):
+    cleaned = re.sub(r'\s+\n\|', ' |', input_string)
+    cleaned = re.sub(r'\|\n+\s+', '| ', cleaned)
+    cleaned = re.sub(r'\n\s+', '\n ', cleaned)
+    cleaned = re.sub(r'::= \n\s*', '::= ', cleaned)
+    cleaned = re.sub(r'\n+', '\n', cleaned)
+    cleaned = re.sub(r'^\s+|\n\s+', '\n', cleaned)
+    return cleaned
 
-def create_tag_dynamic_bnf(file_path, tags_to_keep, file_output_path, print_result = False):
+def create_tag_dynamic_bnf(file_path, tags_to_keep, print_result = False):
   split_content = split_file_by_pattern(file_path)
   processed_tuples = process_tuples(split_content, tags_to_keep)
   final_bnf = join_tuples(processed_tuples)
-  save_string_to_file(final_bnf, file_output_path)
+  final_bnf = clean_string(final_bnf)
+  save_string_to_file(final_bnf, os.path.join('..', 'grammars', file_path.replace('.bnf', '_complete_dynamic.bnf')))
   if print_result:
     print(final_bnf)
-
-"""
-Example usage:
-
-file_path = "input.bnf"
-tags_to_keep = ["<predefined>", "<DECORATORS>", "<NEWLINE>", "<code>", "<vars>", "<var>", "<IMPORTS>"]
-file_output_path = "output.bnf" 
-
-create_tag_dynamic_bnf(file_path, tags_to_keep, file_output_path, False)
-
-
-"""
